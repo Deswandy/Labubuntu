9b1e09d511023fae2e9a6d17cc3e4d183519e9c9
Deswandy <deswandywong23@gmail.com>
2025-07-25 16:31:47 +0700

    made the sidebar

---
+++ src/app/dashboard/add/page.tsx
@@ -0,0 +1,15 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
+  return (
+    <section>
+        Nigga
+    </section>
+  )
+}
